cf481a54c7c825df765ef95f0bd60750a3e544df
peace <peacebassey30@gmail.com>
2023-11-16 12:41:24 +0100

    message-page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 AOS.init({duration:2000});
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-button')) {
+    var dropdownContent = document.getElementById("dropdownContent");
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    }
+  }
+}
