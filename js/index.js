479d97272559ced21e745042a25fb69c963f90ee
peace <peacebassey30@gmail.com>
2023-12-12 03:12:44 +0100

    new changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
-AOS.init({duration:2000});
+// AOS.init({duration:2000});
+const mobileMenuEl = document.querySelector('.mobileNavMenu');
+const openEl = document.querySelector('.burger-menu');
+const closeEl = document.querySelector('.close');
+openEl.addEventListener('click', () => {
+    mobileMenuEl.classList.toggle("open");
+    
+});
+closeEl.addEventListener('click', () => {
+    mobileMenuEl.classList.remove("open");
+    console.log('clicked');
+});
+
+
 function toggleDropdown() {
   var dropdownContent = document.querySelectorAll(".dropdown-content");
   dropdownContent.forEach(n=> {
