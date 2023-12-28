fb89eef40ec0b19674a2d2254e013b033a92baec
peace <peacebassey30@gmail.com>
2023-12-28 14:09:34 +0100

    new

--- js/index.js
+++ js/index.js
@@ -32,3 +32,35 @@ window.onclick = function(event) {
   }
 };
 
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the search input element
+  var searchInput = document.querySelector('.search-input');
+
+  // Get all the transcript items
+  var transcriptItems = document.querySelectorAll('.message-section2-content-container');
+
+  // Add click event listener to the search image
+  var searchImage = document.querySelector('.search-container .fa-search');
+  searchImage.addEventListener('click', function() {
+    // Get the search term from the input
+    var searchTerm = searchInput.value.trim().toLowerCase();
+
+    // Loop through all the transcript items
+    for (var i = 0; i < transcriptItems.length; i++) {
+      var transcriptItem = transcriptItems[i];
+
+      // Get the transcript text
+      var transcriptText = transcriptItem.innerText.toLowerCase();
+
+      // Show or hide the transcript item based on the search term
+      if (transcriptText.includes(searchTerm)) {
+        transcriptItem.style.display = 'block';
+      } else {
+        transcriptItem.style.display = 'none';
+      }
+    }
+  });
+});
+
+
